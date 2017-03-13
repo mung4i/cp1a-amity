c2cf579ffe128d2d6a317d84d6a966032e329e7d
mungaiandela <martin.mungai@andela.com>
2017-03-13 19:23:41 +0300

    [Feature] Create room test

--- tests/test_amity.py
+++ tests/test_amity.py
@@ -13,3 +13,10 @@ class TestAmity(unittest.TestCase):
         for i in ["Martin", "asdas", "asdgfdg"]:
             self.new_fellow = Fellow(i)
             self.amity.people["FELLOWS"].append(self.new_fellow)
+
+    def test_create_room_and_add_room_successfully(self):
+        # function tests if the create room method is adding rooms to lists
+        # Create rooms which can offices or living spaces
+        self.amity.create_room(["Java", "Python"], 'LivingSpace')
+        self.assertIn("Java", self.amity.rooms['LivingSpace'],
+                      msg="Rooms were not created")
