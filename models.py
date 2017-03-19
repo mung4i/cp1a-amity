9f40cffd7e148998ee43edfb5e08e25c15850352
mungaiandela <martin.mungai@andela.com>
2017-03-19 12:06:11 +0300

    [Feature] Add models.py file containing ORM models for amity

---
+++ models.py
@@ -0,0 +1,53 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Offices(Base):
+    __tablename__ = "Offices"
+    office_id = Column(Integer(25), primary_key=True)
+    office_name = Column(String(25))
+    room_capacity = Column(Integer(5))
+
+
+class LivingSpaces(Base):
+    __tablename__ = "LivingSpaces"
+    livingspace_id = Column(Integer(25), primary_key=True)
+    livingspace_name = Column(String(25))
+    room_capacity = Column(Integer(5))
+
+
+class Fellows(Base):
+    __tablename__ = "Fellows"
+    fellow_id = Column(Integer(25), primary_key=True)
+    fellow_name = Column(String(25))
+    person_type = Column(String(25))
+    wants_space = Column(String(5))
+
+
+class Staff(Base):
+    __tablename__ = "Staff"
+    staff_id = Column(Integer(25), primary_key=True)
+    staff_name = Column(String(25))
+    person_type = Column(String(25))
+
+
+class Allocated(Base):
+    __tablename__ = "Allocated_fellows"
+    livingspace_name = Column(String(25), primary_key=True)
+    allocated_fellows = Column(String(50))
+
+
+class Unallocated(Base):
+    __tablename__ = "Unallocated_fellows"
+    fellow_name = Column(String(50), primary_key=True)
+    person_type = Column(String(10))
+    wants_space = Column(String(5))
+
+
+def create_db(dbname):
+    db_dir = "/"
+    engine = create_engine('sqlite:///' + db_dir + dbname + '.db')
+    return Base.metadata.create_all(engine)
