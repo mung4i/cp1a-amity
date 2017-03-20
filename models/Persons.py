633f107b84e89ea9ee0ea525ca77816ea981e358
mungaiandela <martin.mungai@andela.com>
2017-03-20 15:06:49 +0300

    [Chore] Change name to first name and last name

--- models/Persons.py
+++ models/Persons.py
@@ -4,6 +4,7 @@
 
 class Persons(object):
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
         self.employeeID = id(self)
