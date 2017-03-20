018a8b818f9cc7600b1590b996b581bcdbaef0c5
mungaiandela <martin.mungai@andela.com>
2017-03-20 15:06:06 +0300

    [Chore] Change name to first and last name

--- models/Fellow.py
+++ models/Fellow.py
@@ -7,6 +7,6 @@ from Persons import Persons
 class Fellow(Persons):
     wants_space = "N"
 
-    def __init__(self, name):
-        super(Fellow, self).__init__(name)
+    def __init__(self, first_name, last_name):
+        super(Fellow, self).__init__(first_name, last_name)
         self.allocated = False
