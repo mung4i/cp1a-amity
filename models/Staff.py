bcfff69d64ea2ad11d9f9f5c0dfed6ac1490529e
mungaiandela <martin.mungai@andela.com>
2017-03-20 15:07:43 +0300

    [Chore] Change to first name and last name

--- models/Staff.py
+++ models/Staff.py
@@ -6,5 +6,5 @@ from Persons import Persons
 
 class Staff(Persons):
 
-    def __init__(self, name):
-        super(Staff, self).__init__(name)
+    def __init__(self, first_name, last_name):
+        super(Staff, self).__init__(first_name, last_name)
