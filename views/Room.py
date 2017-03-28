616e963abd4bac425a20f71ed62c3fdde8b806fb
mungaiandela <martin.mungai@andela.com>
2017-03-28 16:59:43 +0300

    [Feature] Add repr function to print room objects represented as strings

--- views/Room.py
+++ views/Room.py
@@ -8,11 +8,14 @@ class Rooms(object):
     def __init__(self, room_name):
         self.room_id = id(self)
         self.room_name = room_name
+        self.occupants = []
+
+    def __repr__(self):
+        return self.room_name
 
 
 class LivingSpace(Rooms):
     room_capacity = 4
-    occupants = []
 
     def __init__(self, room_name):
         super(LivingSpace, self).__init__(room_name)
