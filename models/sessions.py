23629fdf5fad5606110eeefe96ff1c4386c0dcd5
mungaiandela <martin.mungai@andela.com>
2017-03-29 15:19:12 +0300

    [Feature] Populate allocated and load allocations fixed

--- models/sessions.py
+++ models/sessions.py
@@ -32,7 +32,8 @@ class Sessions(object):
 
             else:
                 self.session.commit()
-                print "Success! Offices have been synced"
+
+        print "Success! Offices have been synced"
 
     def populate_livingspaces(self):
         for room in Amity.rooms["LivingSpace"]:
@@ -49,7 +50,8 @@ class Sessions(object):
 
             else:
                 self.session.commit()
-                print "Success! Living spaces have been synced"
+
+        print "Success! Living spaces have been synced"
 
     def populate_fellows(self):
         for fellow in Amity.people["FELLOWS"]:
@@ -69,7 +71,7 @@ class Sessions(object):
 
             else:
                 self.session.commit()
-                print "Success! Fellows have been synced"
+        print "Success! Fellows have been synced"
 
     def populate_staff(self):
         for staff in Amity.people["STAFF"]:
@@ -85,41 +87,56 @@ class Sessions(object):
                 self.session.commit()
                 print "{0} {1} has been added to the database".format(
                     staff.first_name, staff.last_name)
-
-            elif staff_object:
+            else:
                 self.session.commit()
-                print "Success! Staff have been synced"
+        print "Success! Staff have been synced"
 
     def populate_allocated(self):
-        for allocated in Amity.allocated_persons:
-            allocate = self.session.query(Allocated).filter_by(
-                id=allocated[0].employeeID,
-                livingspace_name=allocated[1].room_name,
-                allocated_fellows_fname=allocated[0].first_name,
-                allocated_fellows_lname=allocated[0].last_name).first()
+        # try:
+        for rooms in Amity.rooms["Office"]:
+            staff = rooms.occupants
+            for staff_object in staff:
+                allocated_persons = self.session.query(Allocated).filter_by(
+                    first_name=staff_object.first_name,
+                    last_name=staff_object.last_name).first()
+                if allocated_persons is None:
+                    if staff_object.role == 'staff':
+                        allocated = Allocated(id=staff_object.employeeID,
+                                              first_name=staff_object.first_name,
+                                              last_name=staff_object.last_name,
+                                              person_type="STAFF",
+                                              office=rooms.room_name,
+                                              livingspace="None")
+                    if staff_object.role == 'fellow':
+                        allocated = Allocated(id=staff_object.employeeID,
+                                              first_name=staff_object.first_name,
+                                              last_name=staff_object.last_name,
+                                              person_type="FELLOWS",
+                                              office=rooms.room_name)
+                    self.session.add(allocated)
+                    self.session.commit()
+                print "{0} {1} has been added to the database".format(
+                    staff_object.first_name, staff_object.last_name)
 
-            if allocate is None:
-                add_allocations = Allocated(
-                    id=allocated[0].employeeID,
-                    livingspace_name=allocated[1].room_name,
-                    allocated_fellows_fname=allocated[0].first_name,
-                    allocated_fellows_lname=allocated[0].last_name)
-                self.session.add(add_allocations)
-                self.session.commit()
-                print "{0} {1} allocations' have been recorded".format(
-                    allocated[0].first_name, allocated[0].last_name)
+        for rooms in Amity.rooms["LivingSpace"]:
+            fellows = rooms.occupants
+            for i in fellows:
+                fellows_object = i
+                allocated_persons = self.session.query(Allocated).filter_by(
+                    first_name=fellows_object.first_name,
+                    last_name=fellows_object.last_name,
+                    livingspace=rooms.room_name).first()
+                if allocated_persons is None:
 
-            else:
-                self.session.delete(allocate)
-                self.session.commit()
-                new_allocations = Allocated(
-                    id=allocated[0].employeeID,
-                    livingspace_name=allocated[1].room_name,
-                    allocated_fellows_fname=allocated[0].first_name,
-                    allocated_fellows_lname=allocated[0].last_name)
-                self.session.add(new_allocations)
-                self.session.commit()
-                print "Database is synced"
+                    allocated = Allocated(id=fellows_object.employeeID,
+                                          first_name=fellows_object.first_name,
+                                          last_name=fellows_object.last_name,
+                                          person_type="FELLOWS",
+                                          livingspace=rooms.room_name)
+                    self.session.add(allocated)
+                    self.session.commit()
+                print "{0} {1} has been added to the database".format(
+                    fellows_object.first_name, fellows_object.last_name)
 
     def populate_unallocated(self):
         for unallocated in Amity.unallocated_persons:
@@ -137,14 +154,6 @@ class Sessions(object):
                     unallocated.first_name, unallocated.last_name)
 
             else:
-                self.session.delete(unallocate)
-                self.session.commit()
-                unallocations = Unallocated(id=unallocated.employeeID,
-                                            first_name=unallocated.first_name,
-                                            last_name=unallocated.last_name,
-                                            person_type="FELLOW",
-                                            wants_space="Y")
-                self.session.add(unallocations)
                 self.session.commit()
                 print "Database is synced"
 
@@ -157,11 +166,13 @@ class Sessions(object):
             old_office.room_name = office.office_name
             old_office.room_id = office.office_id
             Amity.rooms["Office"].append(old_office)
+            print Amity.rooms["Office"]
 
         for livingspace in livingspaces:
             old_lspace = LivingSpace(livingspace.livingspace_name)
             old_lspace.room_id = livingspace.livingspace_id
             Amity.rooms["LivingSpace"].append(old_lspace)
+            print Amity.rooms["LivingSpace"]
 
         print "Rooms loaded successfully"
 
@@ -188,17 +199,28 @@ class Sessions(object):
         unallocations = self.session.query(Unallocated).all()
 
         for allocated in allocations:
-            room = allocated.livingspace_name
-            room_obj = self.amity.get_roomobject(room)
-            person_obj = Fellow(allocated.allocated_fellows_fname,
-                                allocated.allocated_fellows_lname)
-            person_obj.employeeID = allocated.id
-            Amity.allocated_persons.append([person_obj, room_obj])
-            room_obj.occupants.append(person_obj)
+            livingspace = allocated.livingspace
+            office = allocated.office
+            for lspace in Amity.rooms["LivingSpace"]:
+                if lspace.room_name == livingspace:
+                    person_obj = self.amity.get_fellowobject(
+                        allocated.first_name, allocated.last_name)
+                    lspace.occupants.append(person_obj)
+            for ospace in Amity.rooms["Office"]:
+                if ospace.room_name == office:
+                    if allocated.person_type == "FELLOWS":
+                        person_obj = self.amity.get_fellowobject(
+                            allocated.first_name, allocated.last_name)
+                        ospace.occupants.append(person_obj)
+                    if allocated.person_type == "STAFF":
+                        staff_obj = self.amity.get_staffobject(
+                            allocated.first_name, allocated.last_name)
+                        ospace.occupants.append(staff_obj)
 
         for unallocated in unallocations:
             person_obj = Fellow(unallocated.first_name, unallocated.last_name)
             person_obj.employeeID = unallocated.id
             Amity.unallocated_persons.append(person_obj)
+            Amity.people["FELLOWS"].append(person_obj)
 
         print "Allocations loaded successfully"
